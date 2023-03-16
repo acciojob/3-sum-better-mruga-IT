3b16e53df5f7e86a287bb2ea74e40bb9a40d352e
mruga-IT <124659354+mruga-IT@users.noreply.github.com>
2023-03-17 01:07:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
 function threeSum(S, target) {
-  // Sort the array in ascending order
-  S.sort((a, b) => a - b);
-  // Initialize the closest sum to a large number
   let closestSum = Infinity;
-  // Loop through the array, fixing the first element
   for (let i = 0; i < S.length - 2; i++) {
-    // Two pointers approach for the remaining two elements
-    let left = i + 1;
-    let right = S.length - 1;
-    while (left < right) {
-      // Calculate the current sum of the three elements
-      let currentSum = S[i] + S[left] + S[right];
-      // Update the closest sum if the current sum is closer to the target
-      if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
-        closestSum = currentSum;
-      }
-      // Move the pointers according to the current sum
-      if (currentSum < target) {
-        left++;
-      } else {
-        right--;
+    for (let j = i + 1; j < S.length - 1; j++) {
+      for (let k = j + 1; k < S.length; k++) {
+        let currentSum = S[i] + S[j] + S[k];
+        if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+          closestSum = currentSum;
+        }
       }
     }
   }
-  // Return the closest sum found
   return closestSum;
 }
-This function uses a two pointers approach to find the three integers in the array S whose sum is closest to the given target number. We first sort the array in ascending order and initialize the closest sum to a large number (in this case, Infinity).
+This implementation uses a brute force approach to find the three integers in the array S whose sum is closest to the given target number. It loops through all possible combinations of three integers in the array and calculates their sum. It then updates the closest sum found so far if the current sum is closer to the target.
 
-We then loop through the array, fixing the first element and using two pointers to iterate through the remaining two elements. We calculate the current sum of the three elements, update the closest sum if the current sum is closer to the target, and move the pointers accordingly based on the current sum.
-
-Finally, we return the closest sum found.
-
-Here's an example of how to use the function:
+You can use this updated implementation in the same way as the previous one:
 
 javascript
+Copy code
+let S = [-1, 2, 1, -4];
+let target = 1;
+let closestSum = threeSum(S, target);
+console.log(closestSum); // Output: 2
+In this example, the input array S contains the integers [-1, 2, 1, -4] and the target number is 1. The threeSum function returns the sum of the three integers in S whose sum is closest to target, which is 2 (-1 + 2 + 1 = 2). We store this value in the variable closestSum and then log it to the console.
+
+
+
+
+
+
+   
