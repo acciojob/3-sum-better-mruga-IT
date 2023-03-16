e54714219743d4a256c991a14c9427d2331904f3
mruga-IT <124659354+mruga-IT@users.noreply.github.com>
2023-03-17 01:07:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,17 +12,6 @@ function threeSum(S, target) {
   }
   return closestSum;
 }
-This implementation uses a brute force approach to find the three integers in the array S whose sum is closest to the given target number. It loops through all possible combinations of three integers in the array and calculates their sum. It then updates the closest sum found so far if the current sum is closer to the target.
-
-You can use this updated implementation in the same way as the previous one:
-
-javascript
-Copy code
-let S = [-1, 2, 1, -4];
-let target = 1;
-let closestSum = threeSum(S, target);
-console.log(closestSum); // Output: 2
-In this example, the input array S contains the integers [-1, 2, 1, -4] and the target number is 1. The threeSum function returns the sum of the three integers in S whose sum is closest to target, which is 2 (-1 + 2 + 1 = 2). We store this value in the variable closestSum and then log it to the console.
 
 
 
